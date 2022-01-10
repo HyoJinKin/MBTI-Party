6009c786e7f9cf3fef0746d431d34214b0c5d5eb
Maya <suzy.maya.yu@gmail.com>
2022-01-10 14:56:47 +0900

    initial version

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+# db-setup
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/detail')
+def detail():
+    return render_template('detail.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/build_party')
+def build_party():
+    return render_template('build_party.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5001, debug=True)
+
+
