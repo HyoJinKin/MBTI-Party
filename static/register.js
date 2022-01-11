ad0cfa5ba5c4e779c39dfeb0c7d6d77883660ab5
JerryAllMighty <dbsalszz@naver.com>
2022-01-11 17:14:04 +0900

    아이디 검열 기능 추가 (빈 공간, 특수문자) 아이디 중복 체크 기능 마무리

---
+++ static/register.js
@@ -0,0 +1,97 @@
+function insertEmailInput() {
+    // selectedValue = $('ID-Address').val();
+    // if (selectedValue == "직접 입력") {
+    let temp_html = `<input type="text" id="emailInputBox" placeholder="예) asdtyu@empas.com">`;
+    $('.ID').append(temp_html);
+    // }
+}
+
+
+function registerUser() {
+    let emailAddress = $('#ID-Address option:selected').val()
+    $.ajax({
+        type: "POST",
+        url: "/register",
+        data: {
+            name_give: $('#name').val(),
+            regisNum_give: $('#regisNum').val(),
+            id_give: $('#ID').val() + emailAddress,
+            password_give: $('#password').val(),
+            MBTI_give: $('#MBTI').val()
+
+        },
+        success: function (response) {
+            alert(response['msg'])
+
+        }
+    })
+    // window.location.reload();
+}
+
+function is_nickname(asValue) {
+    var regExp = /^(?=.*[a-zA-Z])[-a-zA-Z0-9_.]{2,10}$/;
+    return regExp.test(asValue);
+}
+
+function is_password(asValue) {
+    var regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,20}$/;
+    return regExp.test(asValue);
+}
+
+
+
+function is_password_thesame(){
+alert('test')
+}
+
+pOrF = false;
+
+function check_dup() {
+    let username = $("#ID").val();
+    console.log(username)
+    if (username == "") {
+        alert("아이디를 입력해주세요.")
+        pOrF = false;
+        $("#ID").focus()
+        return;
+    }
+    if (!is_nickname(username)) {
+        alert("아이디의 형식을 확인해주세요. 영문과 숫자, 일부 특수문자(._-) 사용 가능. 2-10자 길이")
+        pOrF = false;
+        $("#ID").focus()
+        return;
+    }
+
+    checkID();
+}
+
+function checkID() {
+    let id = $('#ID').val();
+    let emailAddress = $('#ID-Address option:selected').val();
+    ;
+    $.ajax({
+        type: "POST",
+        url: "/register/checkid",
+        data: {id_give: id + emailAddress},
+        success: function (response) {
+            console.log(response['msg'])
+            let exists = response['exists']
+            if (exists == true) {
+                alert('중복되는 아이디입니다. 다시 시도해주세요.')
+                $
+                pOrF = false;
+            }
+            if (exists == false) {
+                alert('사용 가능한 아이디입니다.')
+                pOrF = true;
+            }
+        }
+    })
+    // window.location.reload();
+}
+
+function idCheckFirst() {
+    // if
+    registerUser();
+}
+
