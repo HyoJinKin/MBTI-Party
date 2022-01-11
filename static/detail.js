25c4d846e2a7a2100f138120ba669a1de2746bca
Maya <suzy.maya.yu@gmail.com>
2022-01-11 16:19:34 +0900

    detail.js / detail.html 수정

--- static/detail.js
+++ static/detail.js
@@ -3,32 +3,33 @@ $(document).ready(function () {
 });
 
 function showPartyInfo() {
-    let max_member_num = $('#max-member-num').val()
-    let joined_member_id_array = $('#memberIds').split(",")
-    let joined_member_role_array = $('#memberRoles').split(",")
+    let party_id = $('#party-id').val()
 
-    let available_entries = $('#')
+    let favorite_mbti_array = $('#favorite_mbti').val().split(",")
+    for (i=0; i<favorite_mbti_array.length; i++) {
+        let mbti = favorite_mbti_array[i]
+        unit_mbti_html = `<div class="mbti-text">${mbti}</div>`
+        $('#favorite-mbti-entries').append(unit_mbti_html)
+    }
 
+    let member_roles_array = $('#member-roles').val().split(";")
 
-
-    $.ajax({
-        type: 'GET',
-        url: 'api/show_party_info',
-        data: {id_request: partyId},
-        success: function(response) {
-            let partyDetail = response['party'];
-
-        }
-    });
+    for (i=0; i<member_roles_array.length; i++) {
+        role = member_roles_array[i].split(",")[0]
+        user_id = member_roles_array[i].split(",")[1]
+        console.log(role, user_id)
+        joined_html = `<div class="member-entry__role">${role}</div><div class="member-entry__id">${user_id}</div>`
+        $('#member-entries').append(joined_html)
+    }
 }
 
-function joinParty(partyId, userId, role) {
-    $.ajax({
-        type: 'POST',
-        url: 'api/join_party',
-        data: {party_id_request: partyId, user_id_request: userId, role_request: role},
-        success: function(response) {
-            window.location.reload()
-        }
-    });
-}
+// function joinParty(partyId, userId, role) {
+//     $.ajax({
+//         type: 'POST',
+//         url: 'api/join_party',
+//         data: {party_id_request: partyId, user_id_request: userId, role_request: role},
+//         success: function(response) {
+//             window.location.reload()
+//         }
+//     });
+// }
