646e6ddb64e2860e8dd46596cd496372bc0ab9fb
Maya <suzy.maya.yu@gmail.com>
2022-01-11 18:49:11 +0900

    detail.css 수정

--- app.py
+++ app.py
@@ -117,30 +117,30 @@ def registerUser():
 @app.route('/login')
 def login():
     return render_template('login.html')
-#
-# @app.route('/api/login', method=['POST'])
-# def api_login():
-#     id_receive = request.form['id_give']
-#     pw_receive = request.form['pw_give']
-#
-#     # hash 기능으로 pw를 암호화한다.
-#     pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
-#
-#     # id, 암호화된 pw 가지고 있는 유저 찾기.
-#     result = db.users.find_one({'id':id_receive, 'pw':pw_hash})
-#     # 찾으면 JWT 토큰 발급.
-#     if result is not None:
-#         payload = {
-#             'id': id_receive,
-#             'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=2)
-#         }
-#
-#         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
-#
-#         # 만든 토큰을 준다.
-#         return jsonify({'result':'success', 'token':token})
-#     else:
-#         return jsonify({'result': 'fail', 'msg':'아이디 / 비밀번호가 일치하지 않습니다.'})
+
+@app.route('/api/login', method=['POST'])
+def api_login():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+
+    # hash 기능으로 pw를 암호화한다.
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    # id, 암호화된 pw 가지고 있는 유저 찾기.
+    result = db.users.find_one({'id':id_receive, 'pw':pw_hash})
+    # 찾으면 JWT 토큰 발급.
+    if result is not None:
+        payload = {
+            'id': id_receive,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=2)
+        }
+
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+        # 만든 토큰을 준다.
+        return jsonify({'result':'success', 'token':token})
+    else:
+        return jsonify({'result': 'fail', 'msg':'아이디 / 비밀번호가 일치하지 않습니다.'})
 
 
 
