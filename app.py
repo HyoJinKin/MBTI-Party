22a08a1d747ec59e18019a98124e49ece4551864
Maya <suzy.maya.yu@gmail.com>
2022-01-13 14:34:23 +0900

    채팅 모달창 추가

--- app.py
+++ app.py
@@ -328,4 +328,4 @@ def leave(data):
 
 if __name__ == '__main__':
     # app.run('0.0.0.0', port=5001, debug=True)
-    socketio.run(app, host='0.0.0.0', port='5001', debug=True)
+    socketio.run(app, host='0.0.0.0', port='5000', debug=True)
